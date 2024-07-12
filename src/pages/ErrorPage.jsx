7425bc574cde36e8580d16888a87d85a274acf3a
denisse2911 <134969268+denisse2911@users.noreply.github.com>
2024-07-12 00:19:46 -0300

    ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,18 @@
+import "../css/ErrorPage.css";
+
 function ErrorPage() {
   return (
     <>
-      <h1>Error 404 - Page not found</h1>
+      <div class="error-background">
+        <div class="container">
+          <p class="error-sorry">Sorry,</p>
+          <p class="error-text">We couldn't find that page</p>
+          <button class="btn-error-page">Return to home</button>
+        </div>
+        <div class="logo-position">
+          <img src="./images/8.svg" alt="" class="img-fluid" />
+        </div>
+      </div>
     </>
   );
 }

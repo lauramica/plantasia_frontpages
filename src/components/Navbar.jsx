db840dd16b6e1de71ffe718f0f3b08be0afa38d1
Feder17 <federicarodriguez@gmail.com>
2024-07-17 01:27:32 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
         sticky="top"
         className="navbar-plantasia"
       >
-        <Container className="p-sm-0">
+        <Container className="p-sm-0 d-flex">
           <Navbar.Brand as={Link} to="/" className="text-warning">
             <img
               src={`${import.meta.env.VITE_IMAGES_URL}Plantasia_green.svg`}
@@ -32,57 +32,61 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
               className="navbar-logo"
             />
           </Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="navbarNav"
-            className="navbar-hamburger"
-            onClick={handleNavbarCollapse}
-          />
-          <Navbar.Collapse id="navbarNav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/products">
-                Products
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about-this-project">
-                About this project
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          <div className="d-flex align-items-center ms-auto">
-            {loggedCustomer.token ? (
-              <Nav.Link
-                as={Link}
-                to="/profile"
-                className="navbar-anchor d-flex align-items-center me-2"
-              >
-                <p className="proxima-nova-bold m-0 me-2 d-none d-lg-block welcome-text">
-                  Welcome plant lover!
-                </p>
-                <i className="bi bi-person"></i>
-              </Nav.Link>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/login" className="me-2 navbar-anchor">
-                  <p className="proxima-nova-regular m-0">Log In</p>
+          <div className="d-flex navbar-content">
+            <Navbar.Toggle
+              aria-controls="navbarNav"
+              className="navbar-hamburger"
+              onClick={handleNavbarCollapse}
+            />
+            <div className="">
+              <Navbar.Collapse id="navbarNav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/products">
+                    Products
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/about-this-project">
+                    About this project
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+            <div className="d-flex align-items-center ms-auto">
+              {loggedCustomer.token ? (
+                <Nav.Link
+                  as={Link}
+                  to="/profile"
+                  className="navbar-anchor d-flex align-items-center me-2"
+                >
+                  <p className="proxima-nova-bold m-0 me-2 d-none d-lg-block welcome-text">
+                    Welcome plant lover!
+                  </p>
+                  <i className="bi bi-person"></i>
                 </Nav.Link>
-                <Nav.Link as={Link} to="/register" className="me-1 navbar-anchor">
-                  <p className="proxima-nova-regular m-0">Sign Up</p>
-                </Nav.Link>
-              </>
-            )}
-            <Nav.Link className="navbar-anchor" onClick={handleShow}>
-              <i className="bi bi-cart">
-                {cart.length > 0 ? (
-                  <span className="cart-quantity rounded-circle position-absolute top-0 left-0 text-white">
-                    {cart.reduce((acc, product) => acc + product.quantity, 0)}
-                  </span>
-                ) : (
-                  <></>
-                )}
-              </i>
-            </Nav.Link>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/login" className="me-2 navbar-anchor">
+                    <p className="proxima-nova-regular m-0">Log In</p>
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/register" className="me-1 navbar-anchor">
+                    <p className="proxima-nova-regular m-0">Sign Up</p>
+                  </Nav.Link>
+                </>
+              )}
+              <Nav.Link className="navbar-anchor" onClick={handleShow}>
+                <i className="bi bi-cart">
+                  {cart.length > 0 ? (
+                    <span className="cart-quantity rounded-circle position-absolute top-0 left-0 text-white">
+                      {cart.reduce((acc, product) => acc + product.quantity, 0)}
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </i>
+              </Nav.Link>
+            </div>
           </div>
         </Container>
       </Navbar>

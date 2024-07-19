b93199a3d4c1702c9da8ef0aea9ff6385ac0b98d
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-07-19 09:28:11 -0300

    Navbar toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,11 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
   const loggedCustomer = useSelector((state) => state.customer);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+
+  const handleShow = () => {
+    setShow(true);
+    if (navBarCollapse) handleNavbarCollapse();
+  };
 
   return (
     <>
@@ -24,10 +28,9 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
         variant="light"
         sticky="top"
         className="navbar-plantasia"
-        collapseOnSelect
       >
         <Container className="p-sm-0 d-flex">
-          <Navbar.Brand as={Link} to="/" className="text-warning">
+          <Navbar.Brand as={Link} to="/" className="text-warning" onClick={handleNavbarCollapse}>
             <img
               src={`${import.meta.env.VITE_IMAGES_URL}Plantasia_green.svg`}
               alt="Plantasia logo"
@@ -40,6 +43,7 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
                 as={Link}
                 to="/profile"
                 className="navbar-anchor d-flex align-items-center me-2"
+                onClick={handleNavbarCollapse}
               >
                 <p className="proxima-nova-bold m-0 me-2 d-none d-lg-block welcome-text">
                   Welcome plant lover!
@@ -47,14 +51,14 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
                 <i className="bi bi-person navbar-person-icon"></i>
               </Nav.Link>
             ) : (
-              <>
-                <Nav.Link as={Link} to="/login" className="me-2 navbar-anchor">
-                  <p className="proxima-nova-regular m-0">Log In</p>
-                </Nav.Link>
-                <Nav.Link as={Link} to="/register" className="me-1 navbar-anchor">
-                  <p className="proxima-nova-regular m-0">Sign Up</p>
-                </Nav.Link>
-              </>
+              <Nav.Link
+                as={Link}
+                to="/login"
+                className="me-2 navbar-anchor"
+                onClick={handleNavbarCollapse}
+              >
+                <p className="proxima-nova-regular m-0">Log In</p>
+              </Nav.Link>
             )}
             <Nav.Link className="navbar-anchor" onClick={handleShow}>
               <i className="bi bi-cart">
@@ -75,13 +79,28 @@ function CustomNavbar({ navBarCollapse, handleNavbarCollapse }) {
           />
           <Navbar.Collapse id="navbarNav">
             <Nav className="">
-              <Nav.Link as={Link} to="/" className="proxima-nova-regular">
+              <Nav.Link
+                as={Link}
+                to="/"
+                className="proxima-nova-regular"
+                onClick={handleNavbarCollapse}
+              >
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/products" className="proxima-nova-regular">
+              <Nav.Link
+                as={Link}
+                to="/products"
+                className="proxima-nova-regular"
+                onClick={handleNavbarCollapse}
+              >
                 Products
               </Nav.Link>
-              <Nav.Link as={Link} to="/about-this-project/en" className="proxima-nova-bold">
+              <Nav.Link
+                as={Link}
+                to="/about-this-project/en"
+                className="proxima-nova-bold"
+                onClick={handleNavbarCollapse}
+              >
                 About this project
               </Nav.Link>
             </Nav>
